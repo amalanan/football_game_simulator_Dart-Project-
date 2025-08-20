a5127a0f829cef7f16fcecab84d5abe2a1524d2d
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-08-20 21:38:42 +0300

    Update enums.dart

--- bin/models/enums.dart
+++ bin/models/enums.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 enum Position { goalkeeper, defender, midfielder, forward }
 
 enum WeatherCondition { sunny, rainy, snowy, windy, clear }
@@ -13,19 +12,3 @@ enum Strategy {
 }
 
 enum RefereeStyle { lenient, balanced, strict }
-=======
-enum Position { goalkeeper, defender, midfielder, forward }
-
-enum WeatherCondition { sunny, rainy, snowy, windy, clear }
-
-enum Strategy {
-  offensive,
-  defensive,
-  balanced,
-  counterAttack,
-  pressing,
-  parkTheBus,
-}
-
-enum RefereeStyle { lenient, balanced, strict }
->>>>>>> de206b0e561996bd0cafab4897759f879ca8e966
